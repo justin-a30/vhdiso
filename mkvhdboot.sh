79a8280eb1306f0aa9c6b13a08ac14777cad83f4
justin_hson <51263008+justin-a30@users.noreply.github.com>
2024-11-26 18:22:11 +0700

    Update mkvhdboot.sh

--- mkvhdboot.sh
+++ mkvhdboot.sh
@@ -1,10 +1,8 @@
-#!/bin/sh
-
-tree
+#!/bin/bash
 
 option='-d -R -U -max-iso9660-filenames -D'
 
-size=$(stat -c '%s' ./boot/etfsboot.com)
+size=$(stat -c '%s' vhdiso/boot/etfsboot.com)
 if [ $size -eq 4096 ]; then
     loadsize=8
 else
@@ -12,11 +10,6 @@ else
 fi
 
 
-cd ./boot
-tree
-ln ../efi/microsoft/boot/bcd bcd
-tree
-cd ..
-tree
-mkisofs $option -no-emul-boot -boot-load-size $loadsize -b boot/etfsboot.com  -eltorito-alt-boot -no-emul-boot -e  efi.img  -o ventoy_vhdboot.img ./ 
-tree
+ln vhdiso/efi/microsoft/boot/bcd boot/bcd
+
+mkisofs $option -no-emul-boot -boot-load-size $loadsize -b vhdiso/boot/etfsboot.com  -eltorito-alt-boot -no-emul-boot -e  vhdiso/efi.img  -o ventoy_vhdboot.img vhdiso

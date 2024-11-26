eb936736b8a1823fcc0f3b3a6e202d738c58b701
justin_hson <51263008+justin-a30@users.noreply.github.com>
2024-11-26 13:50:38 +0700

    put tree all time for fun (idk the path

--- mkvhdboot.sh
+++ mkvhdboot.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+tree
+
 option='-d -R -U -max-iso9660-filenames -D'
 
 size=$(stat -c '%s' ./boot/etfsboot.com)
@@ -11,7 +13,10 @@ fi
 
 
 cd ./boot
+tree
 ln ../efi/microsoft/boot/bcd bcd
-
+tree
 cd ..
+tree
 mkisofs $option -no-emul-boot -boot-load-size $loadsize -b boot/etfsboot.com  -eltorito-alt-boot -no-emul-boot -e  efi.img  -o ventoy_vhdboot.img ./ 
+tree

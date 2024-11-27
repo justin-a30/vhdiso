09f53b23208a5936160dd2150a00e7bc50fc7423
bomb <51263008+justin-a30@users.noreply.github.com>
2024-11-27 18:39:27 +0700

    use current efi and try

--- mkvhdboot.sh
+++ mkvhdboot.sh
@@ -4,14 +4,14 @@ cd vhdiso
 
 option='-d -R -U -max-iso9660-filenames -D'
 
-size=$(stat -c '%s' ./boot/etfsboot.com)
+size=$(stat -c '%s' boot/etfsboot.com)
 if [[ "$size" -eq "4096" ]]; then
     loadsize="8"
 else
     loadsize="4"
 fi
 
-ln efi/microsoft/boot/bcd boot/bcd
+ln efi/microsoft/boot/BCD boot/BCD
 
 mkisofs $option -no-emul-boot -boot-load-size $loadsize -b boot/etfsboot.com  -eltorito-alt-boot -no-emul-boot -e  efi.img  -o ../ventoy_vhdboot.img ./ 
 
